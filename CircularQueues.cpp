b4da2ee33423230338ad742bc696b66f81786a6a
jaddu17 <shafajaddu@gmail.com>
2024-05-30 10:37:35 +0700

    memgecek jika antrian penuh

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -26,6 +26,13 @@ public:
             cout << "\nQueue overflow\n";
             return;
         }
+
+        // Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
     }
 };
 
