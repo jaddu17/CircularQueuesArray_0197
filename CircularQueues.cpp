2de039d3f716adf6e473afa13c51a85123fe9609
jaddu17 <shafajaddu@gmail.com>
2024-05-30 10:34:35 +0700

    membuat void insert

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -12,6 +12,20 @@ public:
         FRONT = -1;
         REAR = -1;
     }
+
+    void insert()
+    {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        // Cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+
+        }
+    }
 };
 
 int main()
