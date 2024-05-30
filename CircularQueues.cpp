de8d40ed0b6ec60c680d60fccecc7da41350e81a
jaddu17 <shafajaddu@gmail.com>
2024-05-30 10:52:52 +0700

    void remove jika hanya memiliki satu elemen

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -53,6 +53,13 @@ public:
             return;
         }
         cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        // Cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
     }
 };
 
